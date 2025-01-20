b39ad4a81b21e12cd0f8e68c503f75174f7a2c35
gengyudong <gengyudong0155@gmail.com>
2025-01-20 15:59:11 +0800

    Complete Level-3: Mark as Done

--- src/main/Geng.java/Geng.java
+++ src/main/Geng.java/Geng.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Geng {
     public static void main(String[] args) {
-        String[] inputs = new String[100];
+        Task[] taskList = new Task[100];
         int inputCount = 0;
 
         System.out.println("Hey yo! I'm Geng");
@@ -24,11 +24,43 @@ public class Geng {
                     System.out.println("No texts stored yet! Talk to me more!");
                 } else {
                     for (int i = 0; i < inputCount; i++) {
-                        System.out.println(i + 1 + ". " + inputs[i]);
+                        String status = taskList[i].getStatusIcon();
+                        System.out.println(i + 1 + "." + status + " " + taskList[i].getDescription());
                     }
                 }
+            } else if (input.startsWith("mark")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task task = taskList[index];
+
+                    if (index < 0 || index > inputCount) {
+                        System.out.println("Invalid task number. Please enter a number from 1 to 100");
+                    }
+
+                    task.markComplete();
+                    System.out.println("Good Job! I've marked this task as done:");
+                    System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+                } catch (Exception e) {
+                    System.out.println("Invalid command format. Use mark (task no.)");
+                }
+            } else if (input.startsWith("unmark")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task task = taskList[index];
+
+                    if (index < 0 || index > inputCount) {
+                        System.out.println("Invalid task number. Please enter a number from 1 to 100");
+                    }
+
+                    task.markUncomplete();
+                    System.out.println("Oki, I've marked this task not done yet:");
+                    System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+                } catch (Exception e) {
+                    System.out.println("Invalid command format. Use mark (task no.)");
+                }
             } else {
-                inputs[inputCount] = input;
+                Task task = new Task(input);
+                taskList[inputCount] = task;
                 inputCount++;
                 System.out.println("added: " + input);
             }
